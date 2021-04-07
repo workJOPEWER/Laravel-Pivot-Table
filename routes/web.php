bf2fce047db6292d4db49cd472d4daae012a2bb8
workJopewer <arbuztatiana@gmail.com>
2021-04-07 19:22:14 +0300

    create categories routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
 Route::get('post-create', [\App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
 Route::get('post-edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
+
+Route::get('categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
+Route::get('category-create', [\App\Http\Controllers\CategoryController::class, 'create'])->name('categories.create');
+Route::get('category-edit', [\App\Http\Controllers\CategoryController::class, 'edit'])->name('categories.edit');
