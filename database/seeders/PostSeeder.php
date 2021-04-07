2f1871ccdf9dca6b01937ced4a0f67437351fba5
workJopewer <arbuztatiana@gmail.com>
2021-04-07 17:06:51 +0300

    complite seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+		DB::table('posts')->insert([
+			['title' => 'This is 1th post', 'created_at' => now(), 'updated_at' => now()],
+			['title' => 'This is 2nd post', 'created_at' => now(), 'updated_at' => now()],
+			['title' => 'This is 3rd post', 'created_at' => now(), 'updated_at' => now()],
+			['title' => 'This is 4th post', 'created_at' => now(), 'updated_at' => now()],
+		]);
     }
 }
