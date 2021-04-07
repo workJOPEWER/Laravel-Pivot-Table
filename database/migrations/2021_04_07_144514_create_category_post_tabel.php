9e9d15204055aa1854476aa77a2ec8fd477273eb
workJopewer <arbuztatiana@gmail.com>
2021-04-07 17:53:40 +0300

    put fields name

--- database/migrations/2021_04_07_144514_create_category_post_tabel.php
+++ database/migrations/2021_04_07_144514_create_category_post_tabel.php
@@ -6,26 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCategoryPostTabel extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('category_post', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create( 'category_post', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId( 'post_id' )->constrained( 'posts' )->onDelete( 'cascade' );
+			$table->foreignId( 'category_id' )->constrained( 'categories' )->onDelete( 'cascade' );
+			$table->timestamps();
+		} );
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('category_post');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists( 'category_post' );
+	}
 }
